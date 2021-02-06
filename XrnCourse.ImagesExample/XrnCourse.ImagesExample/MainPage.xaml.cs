09473f0a290b5d60f9f92fd12b1ac006085c4304
sigged <sderdeyn@gmail.com>
2021-02-06 15:10:00 +0100

    Revert "added embedded image and Markup Extension to display it using XAML"
    This reverts commit c9f0737f6335042c3c475c8af431a0a422392668.

--- XrnCourse.ImagesExample/XrnCourse.ImagesExample/MainPage.xaml.cs
+++ XrnCourse.ImagesExample/XrnCourse.ImagesExample/MainPage.xaml.cs
@@ -16,15 +16,6 @@ namespace XrnCourse.ImagesExample
         public MainPage()
         {
             InitializeComponent();
-
-            var embeddedImage = new Image { Aspect = Aspect.AspectFill };
-            //get the image by ResourceID
-            embeddedImage.Source = ImageSource.FromResource("XrnCourse.ImagesExample.images.glider.png");
-            //configure absolute layout 
-            AbsoluteLayout.SetLayoutBounds(embeddedImage, new Rectangle(.4, .45, 512, 109));
-            AbsoluteLayout.SetLayoutFlags(embeddedImage, AbsoluteLayoutFlags.PositionProportional);
-            //add image to layout
-            absLayout.Children.Add(embeddedImage);
         }
     }
 }
